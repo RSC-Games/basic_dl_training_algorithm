9c3eb2530f1cd19b2d8bb44a7dc58c521d1f2145
RSC Games <rsc.games.official@gmail.com>
2024-06-12 16:35:17 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#######################################################################################
+#                         Optimized Deep Learning algorithm                           #
+#                                                                                     #
+#    This algorithm is built, with CUDA and numba, to accelerate learning on small    #
+# networks such that their training can be completed in a reasonable amount of time.  #
+# This is the test file where we train the algorithm so it is capable of doing what   #
+# we want (recognize cats).                                                           #
+#                                                                                     #
+#######################################################################################
+
+def main(argv: list) -> int:
+    pass
