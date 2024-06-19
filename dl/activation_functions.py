Fixed module conflicts. Still working on Gradient Descent

dl/activation_functions.py
@@ -2,10 +2,10 @@ from numba import jit
 import numba
 import numpy as np
 
-from data_structures import ActivationStruct
+from dl.data_structures import ActivationStruct
 
 
-@jit(numba.float32(np.ndarray))
+@jit()
 def sigmoid(Z: np.ndarray) -> ActivationStruct:
     """
     Implements the sigmoid activation function.
@@ -30,7 +30,7 @@ def tanh(Z: np.ndarray) -> ActivationStruct:
     raise NotImplementedError("Unimplemented function 'tanh'!")
 
 
-@jit(numba.float32(numba.float32))
+@jit()
 def relu(Z : np.ndarray) -> ActivationStruct:
     """
     Implements the relu activation function.

dl/model.py
@@ -3,8 +3,95 @@ import numpy as np
 from numba import jit
 import numba
 
-from dl.data_structures import ActivationStruct, LinearOutput, ActivationDerivatives
+from dl.data_structures import ActivationStruct, LinearOutput, ActivationDerivatives, NetworkLayer, DataSet
 from dl.activation_functions import relu, sigmoid
+
+
+# Contains the Neural Network, layers, and all nodes.
+class NeuralNetwork:
+    def __init__(self, layer_dims: list[int]):
+        self.layers = []
+
+        for i in range(1, len(layer_dims)):
+            self.layers.append(NetworkLayer(layer_dims[i], layer_dims[i - 1]))
+
+    def get_layer_count(self) -> int:
+        """
+        Supply the layer count of this network to the caller.
+        
+        :returns int: Layer count of this network.
+        """
+        return len(self.layers)
+    
+    def get_layer(self, layer_id: int) -> NetworkLayer:
+        """
+        Supply the requested layer.
+        
+        :returns NetworkLayer: The requested layer.
+        """
+        return self.layers[layer_id]
+
+
+    @jit()
+    def train(self, set: DataSet, learning_rate=0.0075, num_iterations=2500, log=False):
+        """
+        Train a L-layer neural network.
+        
+        :param set: Input dataset.
+        :param learning_rate: Learning rate of gradient descent.
+        :param num_iterations: Iterations for optimization.
+        """
+        AL = set.X
+        print(f"[Info]: Training Nnet model at lr {learning_rate} for {num_iterations} iterations.")        
+        
+        for i in range(0, num_iterations):
+            # Forward propagation.
+            AL = self.model_forward(set.X)
+
+            # Calculate cost.
+            cost = calc_cost(AL, set.Y)
+
+            # Backward propagation.
+            grads = self.model_backward(AL, set.Y)
+
+    
+    @jit()
+    def model_forward(nnet, X: np.ndarray) -> np.ndarray:
+        """
+        Forward propagation step for a n-level neural network. This network
+        is assumed to use RELU nodes up to the output node, which is sigmoid.
+        
+        :param X: Input layer
+        :param nnet: Network layer weights and biases.
+        :returns list: Returns a list of the output activation value and the
+            cached activation values.
+        """
+
+        caches = []
+        A = X
+        L = nnet.get_layer_count()
+
+        for layer in range(1, L):
+            A_prev = A
+            current_layer = nnet.get_layer(layer)
+            activation = sim_layer(A_prev, current_layer.W, current_layer.b, relu)
+            A = activation.A
+            caches.append(activation)
+
+        current_layer = nnet.get_layer(L)
+        activation = sim_layer(A, current_layer.W, current_layer.b, sigmoid)
+        AL = activation.A
+        caches.append(activation)
+
+        # Store the cached activation data for the backprop stage.
+        nnet.last_iter_caches = caches
+
+        return AL
+    
+
+    @jit()
+    def model_backward(self, AL: np.ndarray, Y: np.ndarray):
+        pass
 
 
 @jit()
@@ -72,7 +159,7 @@ def linear_backward(dZ: np.ndarray, activation_cache: ActivationStruct) -> Activ
     lcache = activation_cache.linear_cache
     A_prev = lcache.cache_A
     W = lcache.cache_W
-    b = lcache.cache_b
+    #b = lcache.cache_b
     m = A_prev.shape[1]
 
     recip_m = 1. / m
@@ -81,3 +168,11 @@ def linear_backward(dZ: np.ndarray, activation_cache: ActivationStruct) -> Activ
     dA_prev = np.dot(W.T, dZ)
 
     return ActivationDerivatives(dA_prev, dW, db)
+
+
+def linear_activation_backward(dA: np.ndarray, activation_cache: ActivationStruct, abfunc: Callable) -> ActivationDerivatives:
+    """
+    Backward propagation for the activation layer.
+    
+    :param dA: Derivative of the last input."""
+    pass